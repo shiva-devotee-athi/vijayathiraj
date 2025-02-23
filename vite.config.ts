369c7a26896ef508322025f1b8f8bb1fa99f10ee
vijayathiraj-S <vijayathiraj99@gmail.com>
2025-02-23 22:29:04 +0530

    404 html added

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", // Ensure assets are properly placed
+  },
   css: {
     postcss: {},
   },
